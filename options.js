e9a684b74255bc01bc218a7f7d8ae1e7aefdb416
Jack Boberg <jack.boberg@gmail.com>
2016-01-30 00:14:32 -0500

    Return eslint with options object

--- options.js
+++ options.js
@@ -1,7 +1,9 @@
+const Eslint = require('eslint');
 const Path = require('path');
 const Package = require('./package.json');
 
 module.exports = {
+  eslint: Eslint,
   cmd: 'mod-standard',
   version: Package.version,
   homepage: Package.homepage,
